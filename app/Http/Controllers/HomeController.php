f19236965895fb854be257305a4cab6da94a63b7
yumiaomiao17040177395 <17040177398@163.com>
2019-10-10 11:08:09 +0800

    队列细节描述说明

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -46,9 +46,24 @@ class HomeController extends Controller
 
     }
 
+    /**
+     * 查看队列运行过程
+     * 1、先分发到队列
+     * 2、链接redis客服端查看：
+     *      redis-cli -h 127.0.0.1 -p 6379
+     *      查看redis中队列键 ：keys *
+     * 3、 启动队列处理：（清晰的看到处理过程，成功还是失败）
+     *          php artisan queue:work  --tries=3
+     * 
+     * 安卓redis扩展
+     * composer require predis/predis
+     * 终端翻墙
+     * export http_proxy=http://127.0.0.1:1087
+     */
     public function redisQueue(){
 
         $user = User::find(1);
+        ProcessVerifyEmail::dispatch($user);
 
         ProcessVerifyEmail::dispatch($user)->delay(now()->addMinutes(2));
 
